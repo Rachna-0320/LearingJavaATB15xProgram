df0ae798fc42ce93e683fd7c6b0433bbe187ebbf
Rachna Kumari <rachnakumari0320@gmail.com>
2025-12-26 15:16:28 +0530

    Learing Basic

---
+++ src/ex_02_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,21 @@
+package ex_02_Java_Basics;
+
+/**
+ * multi comments
+ * Code written by Rachna Kumari
+ * I'm learing basic
+ */
+public class Lab002_Comments {
+    /*
+    multi comments can be used for documentation
+    Multi comments do not affect the program
+     */
+    public static void main(String[] args) {
+        // Single line comment
+        // this code will not execute
+        System.out.println("Hello World");
+//        ( cmd + / ) shortcut for comments
+
+
+    }
+}
