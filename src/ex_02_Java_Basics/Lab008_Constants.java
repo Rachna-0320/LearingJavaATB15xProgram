781a56aa3e36f029d3e28a99a075297806c41bc6
Rachna Kumari <rachnakumari0320@gmail.com>
2025-12-27 17:57:18 +0530

    Learing Basics

---
+++ src/ex_02_Java_Basics/Lab008_Constants.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics;
+
+import org.w3c.dom.ls.LSOutput;
+
+import javax.management.MBeanAttributeInfo;
+import java.sql.SQLOutput;
+
+public class Lab008_Constants {
+    public static void main(String[] args) {
+       final float PI = 3.14f;
+//        PI = 4.122f;
+        System.out.println(PI);
+
+
+    }
+
+
+
+}
---
+++ src/ex_02_Java_Basics/Lab009_Char_Literal.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basics;
+
+public class Lab009_Char_Literal {
+    public static void main(String[] args) {
+        char new_line = '\n' ;
+        char tab_line = '\t' ;
+        char back_space = '\b' ;
+        char carriage_return = '\r' ;
+
+//        System.out.println("RachnaKumari");
+//        System.out.println("Rachna"+new_line+"Kumari");  similar \n
+//        (Rachna written in first line and Kumari written in second line)
+//        System.out.println("Rachna"+tab_line+"Kumari"); similar \t -> output = Rachna	Kumari
+//        System.out.println("Rachna"+back_space+"Kumari"); similar \b -> output = RachnKumari
+//        System.out.println("Rachna"+carriage_return+"Kumari"); similar \r -> output = Kumari
+        
+    }
+
+
+}
