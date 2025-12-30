0fec7a075877b405addd6e3927525271440679b5
Rachna Kumari <rachnakumari0320@gmail.com>
2025-12-30 07:53:29 +0530

    Learing Basics

---
+++ src/ex_02_Java_Basics/Lab010_Printf.java
@@ -0,0 +1,24 @@
+package ex_02_Java_Basics;
+
+public class Lab010_Printf {
+    public static void main(String[] args) {
+//        System.out.printf("Hello");
+//        System.out.printf(" Rachna");
+//        printf = print and not move to next line
+//       int a = 9;
+//        System.out.printf("%d*1 = %d%n",a , a*1);
+//        System.out.printf("%d*2 = %d%n",a , a*2);
+//        System.out.printf("%d*3 = %d%n",a , a*3);
+//        System.out.printf("%d*4 = %d%n",a , a*4);
+//        System.out.printf("%d*5 = %d%n",a , a*5);
+//        System.out.printf("%d*6 = %d%n",a , a*6);
+//        System.out.printf("%d*7 = %d%n",a , a*7);
+//        System.out.printf("%d*8 = %d%n",a , a*8);
+//        System.out.printf("%d*9 = %d%n",a , a*9);
+//        System.out.printf("%d*10= %d%n",a , a*10);
+        int a = 10;
+        System.out.printf("value of a %d ",a);
+        System.out.printf("value of a " + a );
+
+    }
+}
