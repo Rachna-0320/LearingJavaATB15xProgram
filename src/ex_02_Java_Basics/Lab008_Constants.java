875c68e9e9264f272ed25bfd91b557b2f25969f5
Rachna Kumari <rachnakumari0320@gmail.com>
2026-02-23 12:49:39 +0530

    Learning java basic - add 2 file

--- src/ex_02_Java_Basics/Lab008_Constants.java
+++ src/ex_02_Java_Basics/Lab008_Constants.java
@@ -7,10 +7,15 @@ import java.sql.SQLOutput;
 
 public class Lab008_Constants {
     public static void main(String[] args) {
-       final float PI = 3.14f;
+      final  float PI = 3.14f;
 //        PI = 4.122f;
         System.out.println(PI);
-
+       final int MAX_A = 10;
+//        MAX_A = 20;
+        System.out.println(MAX_A);
+     final  int A = 5;
+//      A = 10 ;
+        System.out.println(A);
 
     }
 
--- src/ex_02_Java_Basics/Lab009_Char_Literal.java
+++ src/ex_02_Java_Basics/Lab009_Char_Literal.java
@@ -2,10 +2,10 @@ package ex_02_Java_Basics;
 
 public class Lab009_Char_Literal {
     public static void main(String[] args) {
-        char new_line = '\n' ;
-        char tab_line = '\t' ;
-        char back_space = '\b' ;
-        char carriage_return = '\r' ;
+//        char new_line = '\n' ;
+//        char tab_line = '\t' ;
+//        char back_space = '\b' ;
+//        char carriage_return = '\r' ;
 
 //        System.out.println("RachnaKumari");
 //        System.out.println("Rachna"+new_line+"Kumari");  similar \n
@@ -13,7 +13,8 @@ public class Lab009_Char_Literal {
 //        System.out.println("Rachna"+tab_line+"Kumari"); similar \t -> output = Rachna	Kumari
 //        System.out.println("Rachna"+back_space+"Kumari"); similar \b -> output = RachnKumari
 //        System.out.println("Rachna"+carriage_return+"Kumari"); similar \r -> output = Kumari
-        
+//        String name = null;
+//        System.out.println(name);
     }
 
 
