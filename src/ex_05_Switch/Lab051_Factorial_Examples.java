4de1bdd5e801b4a6f11421a1dd4669b7d5b26b50
Rachna Kumari <rachnakumari0320@gmail.com>
2026-03-10 15:08:06 +0530

    Learning java basic - 10/March/2026

--- src/ex_05_Switch/Lab051_Factorial_Examples.java
+++ src/ex_05_Switch/Lab051_Factorial_Examples.java
@@ -1,6 +1,6 @@
 package ex_05_Switch;
 
-public interface Lab051_Factorial_Examples {
+public class Lab051_Factorial_Examples {
     public static void main(String[] args) {
         int fact = 1;
         for (int i = 1 ; i <= 5 ; i++) {
