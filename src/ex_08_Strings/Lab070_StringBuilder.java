4ba12fa695f70cb0975690b1e46697cceb8dad21
Rachna Kumari <rachnakumari0320@gmail.com>
2026-03-16 16:47:17 +0530

    Learning java basic - Array- 16/March/2026

---
+++ src/ex_08_Strings/Lab070_StringBuilder.java
@@ -0,0 +1,49 @@
+package ex_08_Strings;
+
+public class Lab070_StringBuilder {
+    public static void main(String[] args) {
+
+//        StringBuilder sb = new StringBuilder("Rachna");
+//        System.out.println(sb);
+
+//        sb.reverse();
+//        System.out.println(sb);
+//
+        StringBuilder sb1 = new StringBuilder("Hello");
+       sb1.toString();
+        System.out.println(sb1);
+        sb1.reverse();
+        System.out.println(sb1);
+//
+//        StringBuffer sb2 = new  StringBuffer("Kumari");
+//        sb2.reverse();
+//        System.out.println(sb2);
+
+//        String a = new String("Nirav") ;   // not valid
+//        a.reverse();
+//        System.out.println(a);
+
+//        StringBuilder sb3= new StringBuilder("Rachna");
+//        sb3.append("Kumari") ;
+//        System.out.println(sb3);
+//
+//        StringBuffer sb4= new StringBuffer("Rachna");
+//        sb4.append("Kumari") ;
+//        System.out.println(sb4);
+//
+//        StringBuilder sb5= new StringBuilder("Rachna");
+//        sb5.append("Kumari") ;
+//        sb5.reverse();
+//        System.out.println(sb5);
+//
+
+//        StringBuffer sb6= new StringBuffer("Sad");
+//        sb6.append("Happy") ;
+//        System.out.println(sb6);
+//
+//        sb6.replace(0, 3, "Rachna") ;
+//        System.out.println(sb6);
+
+
+    }
+}
