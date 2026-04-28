9792556b4fc1869c4d02dcdc0dfded3a55013098
Rachna Kumari <rachnakumari0320@gmail.com>
2026-04-28 19:30:45 +0530

    LinkedListQueue Programs

--- src/ex_21_Collection_Framework_Queue_Map/Lab0177_Queue.java
+++ src/ex_21_Collection_Framework_Queue_Map/Lab0177_Queue.java
@@ -1,13 +1,12 @@
 package ex_21_Collection_Framework_Queue_Map;
 
-import java.util.Objects;
 import java.util.PriorityQueue;
 
 public class Lab0177_Queue {
     public static void main(String[] args) {
 
-        // FIFO
-        PriorityQueue pq = new PriorityQueue<>();
+        // PriorityQueue (not strict FIFO, uses natural ordering)
+        PriorityQueue<String> pq = new PriorityQueue<>();
         pq.add("Java");
         pq.add("Code");
         pq.add("Program");
@@ -20,6 +19,5 @@ public class Lab0177_Queue {
         System.out.println(pq.poll());
 
         System.out.println(pq);
-
     }
 }
